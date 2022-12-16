9d0414e1c893486b9da4a5226e5433018e6e4d3d
Yusei Kasahara <ykasahara@curucuru.jp>
2022-12-16 12:44:27 +0900

    change: 小数点の0は表示

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,6 +3,7 @@ const config = {
     extends: ['stylelint-config-recommended', 'stylelint-config-recess-order'],
     rules: {
         indentation: 4,
+        'number-leading-zero': 'always',
     },
     ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
 };
