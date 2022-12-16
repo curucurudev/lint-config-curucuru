91da2e0dc9cb75cf2f9379be0d9900e4729cc464
Yusei Kasahara <ykasahara@curucuru.jp>
2022-12-16 15:37:36 +0900

    change: プラグインを使わない

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,7 @@
 /** @type {import('stylelint').Config} */
 const config = {
-    extends: ['stylelint-config-recess-order', 'stylelint-config-recommended-scss'],
+    extends: ['stylelint-config-recess-order'],
+    customSyntax: 'postcss-scss',
     rules: {
         indentation: 4,
         'number-leading-zero': 'always',
